08942d7b7ab1e83678d6a1fe770364c0d1a064de
prodhanm <mustafizur.prodhan@gmail.com>
2023-09-25 16:12:00 -0400

    resized the image to display efficiatnly.

--- image_manip.py
+++ image_manip.py
@@ -16,10 +16,27 @@ with Image.open(filename) as img:
     #img.show()
 
     cropped_img = img.crop((0,0,2500,4000))
-    cropped_img.show()
+    #cropped_img.show()
     crop_size = cropped_img.size
     print(f"crop_img1 = {crop_size}")
     # img.crop = (x,y, w+x, h+y)
     # x,y can be the coordinates to start and w+x or y+h
     # is a way to add the reduced size to the coordinates.
     # This way the image is cropped below the actual size.
+
+    img_rotate = cropped_img.rotate(
+        -90, expand=True, center=(500,500)
+        )
+    #img_rotate.show()
+    # the rotate() takes a geometrical angle as an argument,
+    # and the value can be positive or negative, depending 
+    # on the orientation of the image. The image has been 
+    # #cropped to center the image of the
+    # child at the tuple: (500,600). 
+
+    img_resize = img_rotate.resize(
+        (700,500)
+    )
+    img_resize.size
+    img_resize.show()
+    print(f"resized = {img_resize}")
