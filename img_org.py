38f140037b604c9f1f72316e986f5e9422a3cbf4
prodhanm <mustafizur.prodhan@gmail.com>
2023-09-28 12:34:22 -0400

    uploading code to review.

---
+++ img_org.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+dir_name = "./image/"
+
+def img_rename(dir_name):
+    for index, dir in enumerate(os.listdir(dir_name), 1):
+        with Image.open(dir) as filename:
+            if dir.endswith(".jpg"):
+                fn, fext = os.path.splitext(dir)
+                fn.replace("image", "mufassa")
+                dir.os.join(fn,index,fext)
+                print(dir)
+    
+img_rename(dir_name)
