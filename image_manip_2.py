a1992f59873a738a43d9d146aa63d348134374b3
prodhanm <mustafizur.prodhan@gmail.com>
2023-10-06 12:13:43 -0400

    further cleanup was neccessary, for clean coding.

--- image_manip_2.py
+++ image_manip_2.py
@@ -12,15 +12,12 @@ def image_load(dir):
         img = Image.open(file)
         img.load()
         cropped_img = img.crop((0,0,2500,4000))
-        #image_convert = cropped_img.convert("L")
         logo_conv = logo.convert("L")
         #threshold = 50, substituting number on conditional statement
         logo_func = logo_conv.point(lambda x:255 if x > 50 else 0)
-        #logo_resize = logo_func.resize((300,81))
         logo_filter = logo_func.filter(ImageFilter.CONTOUR)
         logo_trans = logo_filter.point(lambda x: 0 if x ==255 else 255)
         cropped_img.paste(logo_trans, (0,0), logo_trans)
-        #image_convert.load()
         #cropped_img.show()
 
 image_load(dir)
