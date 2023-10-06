5166e9ab63db551ad9ac5404ce9199d691d8455a
prodhanm <mustafizur.prodhan@gmail.com>
2023-10-06 08:10:23 -0400

    tested the image to load via functional programming. Test passed.

--- image_manip_2.py
+++ image_manip_2.py
@@ -4,3 +4,11 @@ from PIL import Image, ImageFilter, ImageOps
 filename = "./image/image1.jpg"
 logo = "./image/logo.png"
 dir_file = "./image/"
+
+def image_load():
+    with Image.open(filename) as img, \
+        Image.open(logo) as logo_img:
+        img.load()
+        img.show()
+
+image_load()
