3c3d9231dfd91983ee29f4534154ef2bd0cca87b
prodhanm <mustafizur.prodhan@gmail.com>
2023-10-06 11:19:30 -0400

    the paste function works and the logo is showing. Need to repostion the logo.

--- image_manip_2.py
+++ image_manip_2.py
@@ -6,21 +6,23 @@ dir = Path(r"./image/")
 #logo = "./image/logo.png"
 #dir_file = "./image/"
 
-def image_load():
+def image_load(dir):
+    logo = Image.open(dir/"logo.png")
+    logo.load()
+    logo.show()
     for file in dir.glob("*.jpg"):
         img = Image.open(file)
+        img.load()
         cropped_img = img.crop((0,0,2500,4000))
-        cropped_img.load()
-        cropped_img.show()
+        image_convert = cropped_img.convert("L")
+        logo_conv = logo.convert("L")
+        #threshold = 50, substituting number on conditional statement
+        logo_func = logo_conv.point(lambda x:255 if x > 50 else 0)
+        logo_resize = logo_func.resize((150,50))
+        logo_filter = logo_resize.filter(ImageFilter.CONTOUR)
+        logo_trans = logo_filter.point(lambda x: 0 if x ==255 else 255)
+        image_convert.paste(logo_trans, (300,300), logo_trans)
+        #image_convert.load()
+        image_convert.show()
 
-image_load()
-
-'''
-def image_crop():
-    cropped_img = image_load().crop((0,0,2500,4000))
-    cropped_img.show()
-
-image_crop()
-'''
-
-
+image_load(dir)
